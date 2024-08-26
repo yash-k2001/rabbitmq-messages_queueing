d6e62decc133fcf1bdf33bbe98922a22ce74acb7
Yash Kashyap <yashkashyap1205@gmail.com>
2024-08-26 16:46:56 +0530

    Update secured.route.js

--- src/routes/secured.route.js
+++ src/routes/secured.route.js
@@ -4,6 +4,5 @@ const minio = require('../controllers/minIO');
 const upload = require('../middleware/multer');
 
 router.post('/uploadFile', upload.single('files'), minio.uploadToMinio);
-router.get('/fetchFile', minio.fetchMinIo);
 
 module.exports = router 
